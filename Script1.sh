a859c506e700ae7bb079998ca677c91b6af344ed
yogeshb1407 <165278429+yogeshb1407@users.noreply.github.com>
2024-04-02 22:35:51 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,5 +2,4 @@
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
-echo "We are learning Git"
-echo "We are trying this practical from linux EC2"
+
